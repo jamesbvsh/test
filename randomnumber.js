31b63a7eb4e087ecd88da0070a9091326864b660
JBv <jamesbvsh@gmail.com>
2015-12-16 12:23:24 -0500

    Start Test

---
+++ randomnumber.js
@@ -0,0 +1,44 @@
+
+
+/*Using Workspaces, create a Random Number Generator!
+See if you can build a program that collects a number from a user,
+then prints out a random number from 1 to the number the user selects.
+Now is the opportunity to practice using some of the commands you learned in this course.*/
+
+
+var theNumber = parseInt(prompt("Pick a number"))
+oneToNum = Math.random()
+
+console.log(oneToNum, 'is a random number less than', theNumber)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
